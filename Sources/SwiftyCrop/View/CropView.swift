5c09a65c23bd05199b0b1dd0731ea3284be4c6d2
benedom <31181527+benedom@users.noreply.github.com>
2026-08-07 10:16:36 +0200

    Fix button colors not showing

--- Sources/SwiftyCrop/View/CropView.swift
+++ Sources/SwiftyCrop/View/CropView.swift
@@ -248,6 +248,7 @@ struct CropView: View {
         .foregroundStyle(configuration.colors.cancelButton)
       }
       .disabled(isCropping)
+      .tintedGlassEffect(configuration.colors.cancelButtonBackground)
     }
     if configuration.rotateImageWithButtons {
       RotationControlsView(
@@ -299,7 +300,7 @@ struct CropView: View {
         .foregroundStyle(configuration.colors.saveButton)
       }
       .disabled(isCropping)
-      .tintedGlassEffect()
+      .tintedGlassEffect(configuration.colors.saveButtonBackground)
     }
   }
 
@@ -449,6 +450,7 @@ struct RotationControlsView: ToolbarContent {
           .padding()
           .presentationCompactAdaptation(.popover)
         }
+        .tintedGlassEffect(configuration.colors.rotateButtonBackground)
       } else {
         Menu {
           rotationButtons()
@@ -478,6 +480,7 @@ struct RotationControlsView: ToolbarContent {
       Label("Rotate Left", systemImage: "rotate.left")
     }
     .foregroundStyle(configuration.colors.rotateButton)
+    .tintedGlassEffect(configuration.colors.rotateButtonBackground)
 
     Button {
       let numberOfFullCircles = Int(angle.degrees / 360)
@@ -491,6 +494,7 @@ struct RotationControlsView: ToolbarContent {
       Label("Reset Rotation", systemImage: "arrow.uturn.backward.circle")
     }
     .foregroundStyle(configuration.colors.resetRotationButton)
+    .tintedGlassEffect(configuration.colors.resetRotationButtonBackground)
     .opacity(isResetDisabled ? 0.3 : 1)
     .disabled(isResetDisabled)
 
@@ -503,6 +507,7 @@ struct RotationControlsView: ToolbarContent {
       Label("Rotate Right", systemImage: "rotate.right")
     }
     .foregroundStyle(configuration.colors.rotateButton)
+    .tintedGlassEffect(configuration.colors.rotateButtonBackground)
   }
 
   private var isResetDisabled: Bool {
